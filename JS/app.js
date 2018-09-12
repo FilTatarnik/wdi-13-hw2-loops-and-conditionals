4b9342b6fc66d4d1bd10090e7c1236ef1501b12e
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-12 17:57:19 -0500

    Commit 2 - Get Even answered

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,11 @@ console.log("hello");
 for (let i = 1; i<=20; i++){
 	console.log(i);
 }
+
+//write a FOR loop that will only log the even numbers in 0 through 200
+
+for (let i = 2; i<=200; i+=2) {
+	if(i % 2 === 0){	
+	console.log(i);
+}
+}
