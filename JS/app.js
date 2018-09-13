fab830621be82020696876d608eb1589c40cf18b
Filip Tatarnik <ftatarnik@gmail.com>
2018-09-12 21:41:52 -0500

    Commit 5 - getting to know you answered

--- JS/app.js
+++ JS/app.js
@@ -19,47 +19,72 @@ console.log("hello");
 // "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 
 
-//loop that logs "Love me, pet me! HSSSSSS!" 20 times
+// // loop that logs "Love me, pet me! HSSSSSS!" 20 times
+// const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+// const randomValue = cat[Math.floor(3 * Math.random())];
+
+
 // for (let i = 0; i <= 20; i++){
 // 	if(i % 2 === 0){
-// 		console.log("...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away...");
+// 		console.log(Math.random(cat));
 // 	} else 
 // 	//logging the statement "Love me, pet me! HSSSSSS!" everytime the loop runs, 20 times.
 // 	console.log("Love me, pet me! HSSSSSS!");
 
 // }
 
-//^^go back to this, you aren't finished!
+// ^^go back to this, you aren't finished!
 
 
-Write a javascript application that logs all numbers from 1 - 100.
-If a number is divisible by 3 log "Fizz" instead of the number.
-If a number is divisible by 5 log "Buzz" instead of the number.
-If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
-setting up loop
+// setting up loop
 
-for (let i = 1; i < 100; i++) {
-	//if i is divisible by 3 and 5, console log "fizzbuzz" instead of the number
-	if (i % 3 && 5 === 0)
-		console.log("FizzBuzz");
-	//otherwise if i is divisible by 3 console log "fizz"
-	else if(i % 3 === 0)
-		console.log("fizz");
-	//otherwise if i is divisble by 5 console log "buzz"
-	else if(i % 5 === 0)
-		console.log("buzz");
-	//console logging 0-100
-	else console.log(i);	
-}
+// for (let i = 1; i < 100; i++) {
+// 	//if i is divisible by 3 and 5, console log "fizzbuzz" instead of the number
+// 	if (i % 3 && 5 === 0)
+// 		console.log("FizzBuzz");
+// 	//otherwise if i is divisible by 3 console log "fizz"
+// 	else if(i % 3 === 0)
+// 		console.log("fizz");
+// 	//otherwise if i is divisble by 5 console log "buzz"
+// 	else if(i % 5 === 0)
+// 		console.log("buzz");
+// 	//console logging 0-100
+// 	else console.log(i);	
+// }
 
-//Getting to know you
-//Use the following arrays to answer the questions below (name, age, hometown):
-// const shahzad = ["Shahzad", 1000, "Austin"];
-// const jim = ["Jim", 16, "All cities"];
-// const reuben = ["Reuben", 22, "Durham"];
-// const daniel = ["Daniel", 186, "Dallas"];
-// const ryan = ["Ryan", 65, "Denver"];
+// ---------Getting to know you --------- //
+// Use the following arrays to answer the questions below (name, age, hometown):
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
 
-//Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+// Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+shahzad.splice(0, 1, 'Gameboy');
+console.log(shahzad);
+
+//Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
+daniel.splice(1,1, 19);
+console.log(daniel);
+
+//Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+ryan.splice(0, 1, 'Batman');
+ryan.splice(2, 1, 'Gotham City');
+console.log(ryan);
+
+//Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+reuben.splice(2, 1, 'Chicago');
+console.log(reuben);
+
+//Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
+jim.splice(2, 1);
+console.log(jim);
+
+
 
